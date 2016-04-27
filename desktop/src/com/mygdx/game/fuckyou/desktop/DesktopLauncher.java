95ac9ea82b006c356b1400f160da027a8e3e6fa9
NSA <topsecret@nsa.com>
2016-04-27 20:58:30 +0200

    init commit

---
+++ desktop/src/com/mygdx/game/fuckyou/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.game.fuckyou.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.game.fuckyou.fuckyou;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new fuckyou(), config);
+	}
+}
